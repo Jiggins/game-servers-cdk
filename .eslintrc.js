e34f9093d47bb070dd29664aac474ac223a45b11
Jack Higgins <jackhiggins07@gmail.com>
2020-12-29 17:09:26 +0000

    Sort imports in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,16 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    'no-unused-vars': 1,
-    'no-new': 0
+    'no-new': 0,
+
+    // Replace no-unused-vars with TypeScript version
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
+
+    'space-before-function-paren': ['warn', 'never'],
+    'sort-imports': ['warn', {
+      allowSeparatedGroups: true,
+      memberSyntaxSortOrder: ['none', 'all', 'single', 'multiple']
+    }]
   }
 }

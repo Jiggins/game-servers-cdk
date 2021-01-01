747e53294004238c406a43b4d575d1899be24edb
Jack Higgins <jackhiggins07@gmail.com>
2021-01-01 19:06:38 +0000

    Remove Endpoint Subnet because it cost too much

--- lib/networking.ts
+++ lib/networking.ts
@@ -25,20 +25,10 @@ export class Networking extends Construct {
       subnetConfiguration: [
         {
           cidrMask: 28,
-          name: 'Endpoint',
-          subnetType: SubnetType.PRIVATE
-        },
-
-        {
-          cidrMask: 28,
           name: 'GameServers',
           subnetType: SubnetType.PUBLIC
         }
       ]
-    })
-
-    const endpointSubnet = this.vpc.selectSubnets({
-      subnetGroupName: 'Endpoint'
     })
 
     this.securityGroup = new SecurityGroup(this, 'ExternalAccess', {
@@ -49,60 +39,10 @@ export class Networking extends Construct {
 
     this.securityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(25565))
 
-    this.securityGroup.addIngressRule(
-      Peer.ipv4(endpointSubnet.subnets[0].ipv4CidrBlock),
+    this.securityGroup.addEgressRule(
+      Peer.anyIpv4(),
       Port.tcp(443),
-      'Allow HTTPS traffic to the endpoint subnet'
+      'Allow outbound HTTPS traffic'
     )
-
-    endpointSubnet.subnets.map((subnet) => {
-      this.securityGroup.addEgressRule(
-        Peer.ipv4(subnet.ipv4CidrBlock),
-        Port.tcp(443),
-        'Allow HTTPS traffic to the endpoint subnet'
-      )
-    })
-
-    this.createVpcEndpoints()
-  }
-
-  private createVpcEndpoints(): void {
-    const endpointSecurityGroup = new SecurityGroup(this, 'EndpointSecurityGroup', {
-      securityGroupName: 'Endpoint Security Group',
-      description: 'Allow HTTPS to the Endpoint Subnet',
-
-      // TODO: limit egress rules to only the vpc
-      vpc: this.vpc,
-      allowAllOutbound: true
-    })
-
-    endpointSecurityGroup.addIngressRule(
-      Peer.ipv4(this.vpc.vpcCidrBlock),
-      Port.tcp(443)
-    )
-
-    const interfaceEndpoints = [
-      InterfaceVpcEndpointAwsService.CLOUDWATCH,
-      InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
-      InterfaceVpcEndpointAwsService.ECR,
-      InterfaceVpcEndpointAwsService.ECR_DOCKER,
-      InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-      InterfaceVpcEndpointAwsService.SSM
-    ]
-
-    interfaceEndpoints.forEach((service, index) => {
-      this.vpc.addInterfaceEndpoint(`Endpoint${index}`, {
-        service: service,
-        subnets: { subnetType: SubnetType.PRIVATE },
-        securityGroups: [endpointSecurityGroup]
-      })
-    })
-
-    this.vpc.addGatewayEndpoint('S3Endpoint', {
-      service: GatewayVpcEndpointAwsService.S3,
-      subnets: [
-        { subnetType: SubnetType.PRIVATE }
-      ]
-    })
   }
 }
